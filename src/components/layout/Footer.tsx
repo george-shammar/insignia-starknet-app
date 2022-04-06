dcc93646d8bc06bb22d15f81062ad4066685ef2d
george-shammar <george.gbenle@outlook.com>
2022-04-06 11:19:18 +0000

    Style page

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,19 +1,14 @@
-import { Flex, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <Flex as="footer" width="full" align="center">
-      <Text>
-        {new Date().getFullYear()} -{" "}
-        <Link
-          textDecoration="none !important"
-          outline="none !important"
-          boxShadow="none !important"
-          href="https://github.com/a5f9t4"
-          isExternal
-        >
-          github.com/a5f9t4
-        </Link>
+      <Text d="flex">
+        <Box mr={5}>
+          Insignia
+        </Box>
+        {new Date().getFullYear()}
+        
       </Text>
     </Flex>
   );
