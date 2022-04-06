21dce422c92587a0ea5fa8c742dcf6f4c33d1cd8
george-shammar <george.gbenle@outlook.com>
2022-04-06 12:14:33 +0000

    Create game download page

---
+++ src/pages/download.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
+const Download = () => {
+  return (
+    <div>
+        <Box mb={8} maxW={800} w="full" h="full" d="flex" flexDirection="column" align="center">
+          
+        </Box>
+    </div>
+    
+  );
+};
+
+export default Download;
